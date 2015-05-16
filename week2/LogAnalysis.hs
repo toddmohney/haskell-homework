0a9e0016735bff71ac916af6cc3c7b84e8af8e6f
Todd Mohney <toddmohney@gmail.com>
2015-05-16 08:53:16 -0400

    Implement binary tree builder methods

--- week2/LogAnalysis.hs
+++ week2/LogAnalysis.hs
@@ -11,8 +11,27 @@ parseMessage x = let wordlist = words x in
     ("E":severity:timestamp:message) -> LogMessage (Error (read severity)) (read timestamp) (unwords message)
     _                                -> Unknown (unwords wordlist)
 
+
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
 
 
-{- sortMessages :: [LogMessage] -> MessageTree -}
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) messageTree = messageTree -- ignore unknown messages
+insert logMessage Leaf         = (Node Leaf logMessage Leaf) -- base case, insert new message node
+insert logMessage (Node leftMsg msg rightMsg)
+  | (timestamp logMessage) > (timestamp msg) = Node leftMsg msg (insert logMessage rightMsg)
+  | otherwise                                = Node (insert logMessage leftMsg) msg rightMsg
+  where
+    timestamp :: LogMessage -> TimeStamp
+    timestamp (LogMessage _ ts _) = ts
+    timestamp (Unknown _)         = undefined
+
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+
+
+
+
