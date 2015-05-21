d7bb0db6ace7ef308bb60d6d46a53796c1024a59
Todd Mohney <toddmohney@gmail.com>
2015-05-20 21:22:46 -0400

    Complete sortList and intersperse implementations

--- rwh_exercises/chapter_3.hs
+++ rwh_exercises/chapter_3.hs
@@ -1,5 +1,8 @@
 module MyStuff where
 
+import Data.Ord (comparing)
+import Data.List (sortBy)
+
 -- 1,2
 myLength :: [a] -> Int
 myLength [] = 0
@@ -18,3 +21,20 @@ toPalendrome x = x ++ (reverse x)
 -- 5
 isPalendrome :: (Eq a) => [a] -> Bool
 isPalendrome x = x == (reverse x)
+
+
+-- 6
+sortLists :: [[a]] -> [[a]]
+sortLists x = sortBy (comparing length) x
+
+sortLists2 :: [[a]] -> [[a]]
+sortLists2 x = sortBy compareLength x
+  where compareLength a b = compare (length a) (length b)
+
+
+-- 7
+intersperse :: a -> [[a]]-> [a]
+intersperse _ [] = []
+intersperse _ (x:[]) = x 
+intersperse sep (x:xs) = x ++ [sep] ++ intersperse sep xs
+
