1730495d625e3101b348778f71e076e5153e05f3
Todd Mohney <toddmohney@gmail.com>
2015-05-24 12:25:39 -0400

    Implement renderJSONValue

---
+++ simpleJSON/PutJSON.hs
@@ -0,0 +1,24 @@
+module PutJSON 
+  (
+    renderJSONValue
+  ) where
+
+  import Data.List (intercalate)
+  import SimpleJSON
+
+  renderJSONValue :: JSONValue -> String
+  renderJSONValue (JSONString str) = show str
+  renderJSONValue (JSONNumber num) = show num
+  renderJSONValue (JSONBool True)  = "true"
+  renderJSONValue (JSONBool False) = "false"
+  renderJSONValue JSONNull         = "null"
+
+  renderJSONValue (JSONObject obj) = "{" ++ (renderObjs obj) ++ "}"
+    where renderObjs [] = ""
+          renderObjs xs = intercalate "," (map renderPair xs)
+          renderPair (k, v) = show k ++ ": " ++ renderJSONValue v
+
+  renderJSONValue (JSONArray arr) = "[" ++ (values arr) ++ "]"
+      where values [] = ""
+            values items = intercalate "," (map renderJSONValue items)
+
