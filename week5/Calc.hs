2600f18e09f4d5f103e5821411c62d8b3779d304
Todd Mohney <toddmohney@gmail.com>
2015-05-31 10:32:55 -0400

    Implement evalStr

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -1,7 +1,11 @@
 module Calc where
   import ExprT
+  import Parser
 
   eval :: ExprT -> Integer
   eval (Lit int) = int
   eval (Add exp1 exp2) = (eval exp1) + (eval exp2)
   eval (Mul exp1 exp2) = (eval exp1) * (eval exp2)
+
+  evalStr :: String -> Maybe Integer
+  evalStr str = fmap eval (parseExp Lit Add Mul str)
