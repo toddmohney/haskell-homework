a18315be0a18df2bcce73de28571bd0aaf1ea746
Todd Mohney <toddmohney@gmail.com>
2015-06-03 21:12:04 -0400

    Implement Stream

--- week6/Fibonacci.hs
+++ week6/Fibonacci.hs
@@ -17,3 +17,14 @@ module Fibonacci where
         | n == 1    = 1
         | otherwise = (fibs2 !! (n-2)) + (fibs2 !! (n-1))
 
+  data Stream a = Stream a (Stream a)
+
+  instance Show a => Show (Stream a) where
+    show stream = unwords (map show $ take 20 $ streamToList stream)
+
+  streamToList :: (Stream a) -> [a]
+  streamToList (Stream a stream) = a : streamToList stream
+
+  streamRepeat :: a -> Stream a
+  streamRepeat a = Stream a (streamRepeat a)
+
