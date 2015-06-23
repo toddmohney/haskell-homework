5f759ae04e414710a7353ed1ea14b821e2b5ba53
Todd Mohney <toddmohney@gmail.com>
2015-06-22 21:33:10 -0400

    Implement treeFold

--- week8/Party.hs
+++ week8/Party.hs
@@ -1,20 +1,31 @@
 module Party where
   import Data.Monoid
+  import Data.Tree
   import Employee
 
-  glCons :: Employee -> GuestList -> GuestList
-  glCons employee@(Emp empName empFun) (GL empList listFun) = 
-    GL (employee:empList) (listFun + empFun)
+  instance Monoid GuestList where
+    mempty = GL [] 0
+    mappend = addLists
 
   addLists :: GuestList -> GuestList -> GuestList
   addLists (GL empList1 listFun1) (GL empList2 listFun2) = 
     GL (empList1 ++ empList2) (listFun1 + listFun2)
+
+  glCons :: Employee -> GuestList -> GuestList
+  glCons employee@(Emp empName empFun) (GL empList listFun) = 
+    GL (employee:empList) (listFun + empFun)
 
   moreFun :: GuestList -> GuestList -> GuestList
   moreFun list1@(GL _ fun1) list2@(GL _ fun2)
     | fun1 >= fun2 = list1
     | otherwise    = list2
 
-  instance Monoid GuestList where
-    mempty = GL [] 0
-    mappend = addLists
+  treeFold :: (a -> b -> b) -> b -> Tree a -> b
+  treeFold fun acc t = foldr fun acc (flatten t)
+
+  nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+  nextLevel = undefined
+
+  maxFun :: Tree Employee -> GuestList
+  maxFun = undefined
+
