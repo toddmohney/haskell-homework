489d3cfb4cde3598c9d0d94af6aa9a5013a4b2fb
Todd Mohney <toddmohney@gmail.com>
2015-08-21 22:04:59 -0400

    Reorder for presentation

--- week2/LogAnalysis.hs
+++ week2/LogAnalysis.hs
@@ -2,9 +2,7 @@
 module LogAnalysis where
   import Log
 
-  parse :: String -> [LogMessage]
-  parse = map parseMessage . lines
-
+  -- Exercise 1
   parseMessage :: String -> LogMessage
   parseMessage = parseMessage' . words
     where 
@@ -14,6 +12,33 @@ module LogAnalysis where
       parseMessage' ("E":severity:ts:xs) = LogMessage (Error (read severity)) (read ts) (unwords xs)
       parseMessage' str                  = Unknown (unwords str)
 
+  parse :: String -> [LogMessage]
+  parse = map parseMessage . lines
+  --
+
+  -- Exercise 2
+  insert :: LogMessage -> MessageTree -> MessageTree
+  insert (Unknown _) tree = tree
+  insert logMessage Leaf  = Node Leaf logMessage Leaf
+  insert msg@(LogMessage _ ts _) (Node l msg2@(LogMessage _ ts2 _) r)
+    | ts < ts2  = Node (insert msg l) msg2 r
+    | otherwise = Node l msg2 (insert msg r)
+  insert LogMessage{} (Node _ (Unknown _) _) = undefined -- we must provide this pattern to avoid 
+                                                         -- warnings about non-exhaustive pattern matching
+  --
+
+  -- Exercise 3
+  build :: [LogMessage] -> MessageTree
+  build = foldr insert Leaf
+  --
+
+  -- Exercise 4
+  inOrder :: MessageTree -> [LogMessage]
+  inOrder Leaf = []
+  inOrder (Node l msg r) = inOrder l ++ [msg] ++ inOrder r
+  --
+
+  -- Exercise 5
   whatWentWrong :: [LogMessage] -> [String]
   whatWentWrong = map getMessage . inOrder . build . filter severeError
     where
@@ -27,20 +52,5 @@ module LogAnalysis where
       getMessage :: LogMessage -> String
       getMessage (LogMessage _ _ msg) = msg
       getMessage (Unknown msg)        = msg
-
-  build :: [LogMessage] -> MessageTree
-  build = foldr insert Leaf
-
-  inOrder :: MessageTree -> [LogMessage]
-  inOrder Leaf = []
-  inOrder (Node l msg r) = inOrder l ++ [msg] ++ inOrder r
-
-  insert :: LogMessage -> MessageTree -> MessageTree
-  insert (Unknown _) tree = tree
-  insert logMessage Leaf  = Node Leaf logMessage Leaf
-  insert msg@(LogMessage _ ts _) (Node l msg2@(LogMessage _ ts2 _) r)
-    | ts < ts2  = Node (insert msg l) msg2 r
-    | otherwise = Node l msg2 (insert msg r)
-  insert LogMessage{} (Node _ (Unknown _) _) = undefined -- we must provide this pattern to avoid 
-                                                         -- warnings about non-exhaustive pattern matching
+  --
 
